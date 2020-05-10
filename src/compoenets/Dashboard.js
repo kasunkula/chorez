198fe78b9424f6fe9210d94e688eca7468dd8492
Kasun Kulathilaka <kasunkula@gmail.com>
2020-05-10 17:58:27 +0800

    gcm_sender_id

--- src/compoenets/Dashboard.js
+++ src/compoenets/Dashboard.js
@@ -87,28 +87,6 @@ export default class Dashboard extends React.Component {
         })
     } 
 
-    subscribeToPushManager = () => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js').then(function(reg) {
-              console.log('Service Worker Registered!', reg);
-          
-              reg.pushManager.getSubscription().then(function(sub) {
-                if (sub === null) {
-                    // Update UI to ask user to register for Push
-                    console.log('Not subscribed to push service!');
-                    this.subscribeUser()
-                } else {
-                  // We have a subscription, update the database
-                  console.log('Subscription object: ', sub);
-                }
-              });
-            })
-             .catch(function(err) {
-              console.log('Service Worker registration failed: ', err);
-            });
-          }
-    }
-
     subscribeUser = () => {
         if ('serviceWorker' in navigator) {
           navigator.serviceWorker.ready.then(function(reg) {
